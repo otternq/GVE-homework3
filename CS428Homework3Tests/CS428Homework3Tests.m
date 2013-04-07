a30063ce2935561d3c9719326f9f3696e1c02c39
otternq <otternq@gmail.com>
2013-04-07 01:17:59 -0700

    adding unit test target to project

---
+++ CS428Homework3Tests/CS428Homework3Tests.m
@@ -0,0 +1,32 @@
+//
+//  CS428Homework3Tests.m
+//  CS428Homework3Tests
+//
+//  Created by Nicholas Otter on 4/7/13.
+//  Copyright (c) 2013 otternq. All rights reserved.
+//
+
+#import "CS428Homework3Tests.h"
+
+@implementation CS428Homework3Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CS428Homework3Tests");
+}
+
+@end
